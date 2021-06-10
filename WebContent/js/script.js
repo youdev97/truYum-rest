eb5ec68d144ef595c68bd2cf9725f002f450d03f
Belcaid <940708@cognizant.com>
2021-06-10 15:31:11 +0200

    first commit

---
+++ WebContent/js/script.js
@@ -0,0 +1,20 @@
+
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    let table = document.getElementById("table-items")
+    if(table.rows.length < 2 ) {
+        table.style.display = "none"
+        document.getElementById("notification-msg").innerHTML = 'No items in cart. Use \'Add to Cart\' option in <a href="menu-item-list-customer.html">Menu Item List</a>.'
+        displayNotification()
+    }
+});
+
+
+
+function deleteCartItem() {
+    displayNotification()
+}
+
+function displayNotification() {
+    document.getElementById("notification-msg").style.display = "block"
+}
