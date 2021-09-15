d6aa130e0f0ee9713e2f6fe74cfed3cb6125a3e8
youdev97 <younes.blcd@gmail.com>
2021-09-15 12:45:04 +0200

    REST service app

--- src/main/java/com/cognizant/truyum/controller/CartController.java
+++ src/main/java/com/cognizant/truyum/controller/CartController.java
@@ -3,10 +3,14 @@ package com.cognizant.truyum.controller;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.cognizant.truyum.dto.CartDTO;
@@ -16,42 +20,38 @@ import com.cognizant.truyum.service.CartService;
 
 import lombok.extern.slf4j.Slf4j;
 
-@Controller
 @Slf4j
+@RestController
+@RequestMapping("/carts")
 public class CartController {
 
 	@Autowired
 	CartService cartService;
 
-	@GetMapping(value = "/add-to-cart")
-	public String addToCart(@RequestParam("menuItemId") long menuItemId, RedirectAttributes redirectAttributes) {
+	@PostMapping("/{userId}/{menuItemId}")
+	public void addCartItem(@PathVariable("userId") long userId, @PathVariable("menuItemId") long menuItemId) {
 		log.info("Start");
-		cartService.addCartItem(1, menuItemId);
-		redirectAttributes.addFlashAttribute("addCartStatus", true);
+		cartService.addCartItem(userId, menuItemId);
 		log.info("End");
-		return "redirect:/show-menu-list-customer";
 	}
 
-	@GetMapping(value = "/show-cart")
-	public String showCart(@RequestParam("userId") long userId, ModelMap model) {
+	@GetMapping("/{userId}")
+	public Set<MenuItem> showCart(@PathVariable("userId") long userId) {
 		log.info("Start");
 		CartDTO cart = getAllCartItems(userId);
-		model.addAttribute("cart", cart);
 		log.info("End");
-		return "cart";
+		return cart.getMenuItems();
 	}
 
-	@GetMapping(value = "/remove-cart")
-	public String removeCart(@RequestParam("menuItemId") long menuItemId, @RequestParam("userId") long userId,
-			RedirectAttributes redirectAttributes) {
+	@DeleteMapping("/{userId}/{menuItemId}")
+	public void deleteCartItem(@PathVariable("userId") long userId, 
+			@PathVariable("menuItemId") long menuItemId) {
 		log.info("Start");
 		cartService.removeCartItem(userId, menuItemId);
-		redirectAttributes.addFlashAttribute("removeCartItemStatus", true);
 		log.info("End");
-		return "redirect:/show-cart?userId="+userId;
 	}
 
-	CartDTO getAllCartItems(long userId) {
+	public CartDTO getAllCartItems(long userId) {
 		CartDTO cart = new CartDTO();
 		try {
 			Set<MenuItem> menuItemList = cartService.getAllCartItems(userId);
--- src/main/java/com/cognizant/truyum/controller/MenuItemController.java
+++ src/main/java/com/cognizant/truyum/controller/MenuItemController.java
@@ -3,68 +3,49 @@ package com.cognizant.truyum.controller;
 import java.util.List;
 
 import javax.transaction.SystemException;
-import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.cognizant.truyum.model.MenuItem;
 import com.cognizant.truyum.service.MenuItemService;
 
 import lombok.extern.slf4j.Slf4j;
 
-@Controller
 @Slf4j
+@RestController
+@RequestMapping("/menu-items")
 public class MenuItemController {
 
 	@Autowired
 	MenuItemService menuItemService;
 
-	
-	@GetMapping(value = "/show-menu-list-admin")
-	public String showMenuItemListAdmin(ModelMap model) throws SystemException {
-		log.info("Start");
-		List<MenuItem> menuItemList = menuItemService.getMenuItemListAdmin();
-		model.addAttribute("menuItemList", menuItemList);
-		log.info("End");
-		return "menu-item-list-admin";
-	}
-	
-	@GetMapping(value = "/show-menu-list-customer")
-	public String showMenuItemListCustomer(ModelMap model) throws SystemException {
+	@GetMapping
+	public List<MenuItem> getAllMenuItems() throws SystemException {
 		log.info("Start");
 		List<MenuItem> menuItemList = menuItemService.getMenuItemListCustomer();
-		model.addAttribute("menuItemList", menuItemList);
 		log.info("End");
-		return "menu-item-list-customer";
+		return menuItemList;
 	}
 
-	@GetMapping(value = "/show-edit-menu-item")
-	public String showEditMenuItem(@RequestParam("menuItemId") long menuItemId, ModelMap model) {
+	@GetMapping("/{menuItemId}")
+	public MenuItem getMenuItem(@PathVariable("menuItemId") long menuItemId) {
 		log.info("Start");
 		MenuItem menuItem = menuItemService.getMenuItem(menuItemId);
-		model.addAttribute("menuItemBean", menuItem);
 		log.info("End");
-		return "edit-menu-item";
-
+		return menuItem;
 	}
 
-	@PostMapping(value = "/edit-menu-item")
-	public String editMenuItem(@ModelAttribute("menuItemBean") @Valid MenuItem menuItem, BindingResult bindingResult) {
+	@PutMapping
+	public void modifyMenuItem(@RequestBody MenuItem menuItem) {
 		log.info("Start");
-		if (bindingResult.hasErrors()) {
-			return "edit-menu-item";			
-		} else {
-			menuItemService.modifyMenuItem(menuItem);
-			log.info("End");
-			return "edit-menu-item-status";
-		}
+		menuItemService.modifyMenuItem(menuItem);
+		log.info("End");
 	}
 
 }
